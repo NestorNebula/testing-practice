69c8e678d24283fc3e04cb64b067d13b3b0dd109
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-03 14:51:50 +0200

    feat: add analyzeArray function

--- analyzearray.js
+++ analyzearray.js
@@ -1,1 +1,11 @@
-export function analyzeArray() {}
+export function analyzeArray(array) {
+  const avg = array.reduce((prev, next) => prev + next, 0) / array.length;
+  const max = array.reduce((prev, next) => (prev < next ? next : prev));
+  const min = array.reduce((prev, next) => (prev > next ? next : prev));
+  return {
+    average: avg,
+    length: array.length,
+    max: max,
+    min: min,
+  };
+}
