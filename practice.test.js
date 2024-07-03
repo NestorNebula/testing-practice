de89b8964d5c9df19f2fee1e30459bce518b6caa
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-03 11:21:32 +0200

    test: add sentence test for capitalize

--- practice.test.js
+++ practice.test.js
@@ -3,3 +3,9 @@ import { capitalize } from './capitalize';
 test('capitalize basic string', () => {
   expect(capitalize('light')).toMatch('Light');
 });
+
+test('capitalize sentence', () => {
+  expect(capitalize('there is a tree in the forest.')).toMatch(
+    'There is a tree in the forest.'
+  );
+});
