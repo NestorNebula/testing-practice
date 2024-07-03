f1e179830cdc5f2a9915fa4653c4b6cd3767423b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-03 14:23:15 +0200

    feat: add caesarCipher function

--- caesarcipher.js
+++ caesarcipher.js
@@ -1,1 +1,18 @@
-export function caesarCipher() {}
+export function caesarCipher(string, factor) {
+  const regexp = /([A-Za-z])/;
+  let cipher = '';
+  for (let i = 0; i < string.length; i++) {
+    if (regexp.test(string[i])) {
+      let code = string.charCodeAt(i);
+      for (let j = 0; j < factor; j++) {
+        if (code === 90) code = 65;
+        else if (code === 122) code = 97;
+        else code += 1;
+      }
+      cipher += String.fromCharCode(code);
+    } else {
+      cipher += string[i];
+    }
+  }
+  return cipher;
+}
