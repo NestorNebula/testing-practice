31f4d081c3faa06a1528872517ea1bdcd42983ac
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-07-03 13:35:17 +0200

    test: group each function tests

--- practice.test.js
+++ practice.test.js
@@ -1,44 +1,48 @@
 import { capitalize } from './capitalize';
 import { reverseString } from './reversestring';
 
-test('capitalize basic string', () => {
-  expect(capitalize('light')).toMatch('Light');
+describe('capitalize', () => {
+  test('capitalize basic string', () => {
+    expect(capitalize('light')).toMatch('Light');
+  });
+
+  test('capitalize sentence', () => {
+    expect(capitalize('there is a tree in the forest.')).toMatch(
+      'There is a tree in the forest.'
+    );
+  });
+
+  test('capitalize a string starting with a number', () => {
+    expect(capitalize('60 seconds in a minute')).toMatch(
+      '60 Seconds in a minute'
+    );
+  });
+
+  test('capitalize a string starting with a space', () => {
+    expect(capitalize(' space')).toMatch(' Space');
+  });
+
+  test('capitalize a string starting with a special character', () => {
+    expect(capitalize('$dollar')).toMatch('$Dollar');
+  });
 });
 
-test('capitalize sentence', () => {
-  expect(capitalize('there is a tree in the forest.')).toMatch(
-    'There is a tree in the forest.'
-  );
-});
+describe('reverseString', () => {
+  test('reverse basic string', () => {
+    expect(reverseString('dark')).toMatch('krad');
+  });
 
-test('capitalize a string starting with a number', () => {
-  expect(capitalize('60 seconds in a minute')).toMatch(
-    '60 Seconds in a minute'
-  );
-});
+  test('reverse sentence', () => {
+    expect(reverseString('The cat is sleeping')).toMatch('gnipeels si tac ehT');
+  });
 
-test('capitalize a string starting with a space', () => {
-  expect(capitalize(' space')).toMatch(' Space');
-});
+  test('reverse sentence with numbers', () => {
+    expect(reverseString('He has 20 dollars to buy a gift')).toMatch(
+      'tfig a yub ot srallod 02 sah eH'
+    );
+  });
 
-test('capitalize a string starting with a special character', () => {
-  expect(capitalize('$dollar')).toMatch('$Dollar');
-});
-
-test('reverse basic string', () => {
-  expect(reverseString('dark')).toMatch('krad');
-});
-
-test('reverse sentence', () => {
-  expect(reverseString('The cat is sleeping')).toMatch('gnipeels si tac ehT');
-});
-
-test('reverse sentence with numbers', () => {
-  expect(reverseString('He has 20 dollars to buy a gift')).toMatch(
-    'tfig a yub ot srallod 02 sah eH'
-  );
-});
-
-test('reverse sentence with special characters', () => {
-  expect(reverseString('twenty $ is enough!')).toMatch('!hguone si $ ytnewt');
+  test('reverse sentence with special characters', () => {
+    expect(reverseString('twenty $ is enough!')).toMatch('!hguone si $ ytnewt');
+  });
 });
